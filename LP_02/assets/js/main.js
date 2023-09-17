600f92ef5015572d04f075f2fa1216e712a4e644
DanielBorbafs <dev.danielborba@gmail.com>
2023-09-17 19:15:34 -0300

    Navbar mobile funciionando

--- LP_02/assets/js/main.js
+++ LP_02/assets/js/main.js
@@ -2,23 +2,29 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
-    
-      /* === Menu show ===*/
-    if(navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu')
-        })
-    }
-    if(navClose) {
-        navClose.closest.addEventListener('click', () => {
-            navMenu.classList.remove('show-menu')
-        })
-    }
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== GSAP ANIMATION ===============*/
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
