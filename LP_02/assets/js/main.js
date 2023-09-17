424b96ac1ec038d8dc54ee80a92f6e173d9acd01
DanielBorbafs <dev.danielborba@gmail.com>
2023-09-17 19:06:40 -0300

    Navbar responsivo

--- LP_02/assets/js/main.js
+++ LP_02/assets/js/main.js
@@ -1,5 +1,19 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+    
+      /* === Menu show ===*/
+    if(navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
+    if(navClose) {
+        navClose.closest.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
